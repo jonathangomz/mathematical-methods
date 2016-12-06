0749e4b2c5f2936fff70c19c69bb8c5cfdb5b323
Jonathan Gómez <Jonathan@rorschach.local>
2016-12-06 00:16:29 -0600

    fixed matrix C and parameters for function

--- javascript/matrix.js
+++ javascript/matrix.js
@@ -2,11 +2,15 @@ window.onload = function () {
 
     var A = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
 	var B = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
-    var C = [][];
+    var C = [];
 
-    document.getElementById('boton').onclick = function (A, B) {
+    document.getElementById('boton').onclick = function () {
 
         alert(A);
+
+        for(var i=0; i<3; i++) {
+            C[i] = [];
+        }
 
     }
 
