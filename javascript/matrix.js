9ffaeb9b63afddcc9898fa5692d8ac078bca6457
Jonathan Gómez <Jonathan@rorschach.local>
2016-12-06 00:43:57 -0600

    working with js for matrix

--- javascript/matrix.js
+++ javascript/matrix.js
@@ -6,12 +6,45 @@ window.onload = function () {
 
     document.getElementById('boton').onclick = function () {
 
-        alert(A);
+        multi(A, B);
 
-        for(var i=0; i<3; i++) {
+        addHTML(C);
+    }
+
+    function multi (A, B) {
+        var n = A.length;
+        var n2 = B.length;
+        var m2 = B[0].length;
+
+        for(var i = 0; i < m2; i++) {
             C[i] = [];
+            for(var o = 0; o < n; o++){
+                C[i][o] = 0.0;
+            }
         }
 
+        if(n != n2){
+            return null;
+        }
+
+        for(var i = 0; i < n; i++){
+            for(var o = 0; o < m2; o++){
+                for(var k = 0; k < n; k++){
+                    C[i][o] += A[i][k] * B[k][o];
+                }
+            }
+        }
+    }
+
+    function addHTML (C) {
+        var text = "";
+        for(var i = 0; i < C.length; i++){
+            text += "<tr>";
+            for(var o = 0; o < C[0].length; o++){
+                text += "<td>"+C[i][o]+"</td>";
+            }
+            text += "</tr>";
+        }
     }
 
 }
